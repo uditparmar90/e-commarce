03f3d54f8a2448df968495997135965becefbf45
uditparmar90 <udittparmar@gmail.com>
2024-02-10 22:07:29 +0530

    update feedback form UI

--- myshop/products/forms.py
+++ myshop/products/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class FeedbackForm(forms.Form):
-    name=forms.CharField()
+    name=forms.CharField(widget=forms.TextInput(
+        attrs={'class':'name','id':'uName'}
+    ))
     rating = forms.IntegerField(max_value=5, min_value=1,help_text="Rating must be lessthan 5")
     comment=forms.CharField(widget=forms.Textarea, help_text="Enter your feedback!")
